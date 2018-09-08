a920ccacc6374989fa629de3ac5a99dfd96d117b
Felipe Bergamin <felipebergamin6@gmail.com>
2018-09-08 13:29:30 -0300

    feat: starting with react-router-dom
    put App inside a BrowserRouter component
    and define routes '/' and '/search'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
-import HeaderBar from './HeaderBar';
-import SearchButton from './SearchButton';
+import { Route } from 'react-router-dom';
+
+import ShowBookcases from './ShowBookcases';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HeaderBar />
+        <Route exact path="/" component={ShowBookcases} />
 
-        <SearchButton />
+        <Route exact path="/search" render={() => (
+          <h1>Searching</h1>
+        )} />
       </div>
     );
   }
